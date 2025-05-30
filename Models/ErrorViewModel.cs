4af9e86ee2abcbf5e036afb3c8518504da221d9c
ANDRES DAVID PEREL <48801689@ort>
2025-05-30 15:23:45 -0300

    Base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP05_Perel_Hamu_Turquie.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
